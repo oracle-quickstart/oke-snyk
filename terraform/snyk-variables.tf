5cc64b6f3fb547784863c91ed7bcccd3be0ba8f9
Adao Junior <junior@users.noreply.github.com>
2022-02-17 12:05:31 -0600

    variables

--- terraform/snyk-variables.tf
+++ terraform/snyk-variables.tf
@@ -1,8 +1,13 @@
-# Copyright (c) 2021 Oracle and/or its affiliates. All rights reserved.
+# Copyright (c) 2022 Oracle and/or its affiliates. All rights reserved.
 # Licensed under the Universal Permissive License v 1.0 as shown at http://oss.oracle.com/licenses/upl.
 # 
 
 variable "snyk_integration_id" {
-  default     = true
+  default     = ""
   description = "Snyk Integration Id"
+}
+
+variable "snyk_deploy_goof_sample" {
+  default     = false
+  description = "Deploy Snyk's Goof Sample"
 }
